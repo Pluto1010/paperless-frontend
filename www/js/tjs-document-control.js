ef8ba347395ec2cd4aa276095b0c7540225905ea
Dennis Schulz <dennis@port42.org>
2015-09-26 09:45:29 +0200

    document time traveling prototype works on desktop and IOS 8.4 now

---
+++ www/js/tjs-document-control.js
@@ -0,0 +1,86 @@
+/**
+ * @author dmarcos / http://github.com/dmarcos
+ *
+ * This controls allow to change the orientation of the camera using the mouse
+ */
+
+THREE.DocumentControl = function ( object, domElement, $ionicGesture ) {
+
+  var scope = this;
+  var PI_2 = Math.PI / 2;
+
+  var object = object;
+  var yVector = new THREE.Vector3( 1, 0, 0 );
+
+  var dragOnGoing = false;
+
+  var onDragUpDown = function ( event ) {
+    if ( scope.enabled === false ) return;
+
+    console.log("dragUpDown detected", event);
+    console.log("dragUpDown:event", Object.keys(event));
+
+    var deltaYNegative = event.gesture.deltaY < 0;
+    var deltaY = Math.pow(Math.abs(event.gesture.deltaY), 1.15);
+    if(deltaYNegative) {
+      deltaY *= -1;
+    }
+
+    scope.orientation.yPower = deltaY * 0.04;
+    scope.dragOnGoing = true;
+    event.stopPropagation();
+    return true;
+  };
+
+  var onDragEnd = function ( event ) {
+    if ( scope.enabled === false ) return;
+
+    console.log("dragEnd detected", event);
+    console.log("dragEnd:event", Object.keys(event));
+
+    scope.dragOnGoing = false;
+
+    event.stopPropagation();
+    return true;
+  };
+
+  scope.enabled = true;
+  scope.orientation = {
+    yPower: 0
+  };
+
+  this.update = function() {
+    if ( scope.enabled === false ) return;
+
+    if(!scope.dragOnGoing) {
+      scope.orientation.yPower *= 0.98;
+      if(Math.abs(scope.orientation.yPower) <= 0.0625) {
+        scope.orientation.yPower = 0;
+      }
+    }
+
+    var old = object.position.z;
+    object.translateZ(scope.orientation.yPower);
+
+    if(object.position.z != old) {
+      console.log("Updating object.position.z to " + object.position.z + " with a dY=" + scope.orientation.yPower);
+    }
+
+    //mouseQuat.y.setFromAxisAngle( yVector, this.orientation.y );
+    // mouseQuat.y.setFromAxisAngle( yVector, this.orientation.y );
+    //object.position.y = mouseQuat.y
+    return;
+  };
+
+
+  var dragDownGesture;
+  scope.dispose = function() {
+    $ionicGesture.off(dragDownGesture, 'dragdown', onDragUpDown);
+    $ionicGesture.off(dragUpGesture, 'dragup', onDragUpDown);
+    $ionicGesture.off(dragEndGesture, 'dragend', onDragEnd);
+  }
+
+  dragDownGesture = $ionicGesture.on('dragdown', onDragUpDown, [domElement], {});
+  dragUpGesture = $ionicGesture.on('dragup', onDragUpDown, [domElement], {});
+  dragEndGesture = $ionicGesture.on('dragend', onDragEnd, [domElement], {});
+};
