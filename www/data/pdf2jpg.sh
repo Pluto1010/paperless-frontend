897ef009c9f98c729f280dd47b601af2fd75e82a
Dennis Schulz <Schulz@Dennis-Schulz-MBP.local>
2015-10-05 16:11:01 +0200

    removes trim from PDF convert command. adds reconverted demo documents

--- www/data/pdf2jpg.sh
+++ www/data/pdf2jpg.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 INPUT_FILE_NAME=`basename $1`
-convert -verbose +dither -posterize 16 -alpha remove -colorspace sRGB -density 200 -trim $1 -quality 75 -sharpen 0x1.0 preview/${INPUT_FILE_NAME%.*}.jpg
-
+convert -verbose +dither -posterize 16 -alpha remove -colorspace sRGB -density 200 $1 -quality 75 -sharpen 0x1.0 preview/${INPUT_FILE_NAME%.*}.jpg
